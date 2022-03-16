0b2abed0bd8e9fc4615facbec0b0030ac7e0e85a
sudhakarsn15 <35915646+sudhakarsn15@users.noreply.github.com>
2022-03-16 12:49:50 +0530

    Caluclator

--- src/App.js
+++ src/App.js
@@ -15,22 +15,39 @@ export default function App() {
  * Claculator is Parent class
  */
 class Calculator extends React.Component {
+  render() {
+    return (
+      <div>
+        <TemperatureInput scale="c" />
+        <TemperatureInput scale="f" />
+      </div>
+    );
+  }
+}
+
+const scaleNames = {
+  c: "Celsius",
+  f: "Fahrenheit"
+};
+
+class TemperatureInput extends React.Component {
   constructor(props) {
     super(props);
+    this.handleChange = this.handleChange.bind(this);
     this.state = { temperature: "" };
   }
 
-  handleChange = (e) => {
+  handleChange(e) {
     this.setState({ temperature: e.target.value });
-  };
+  }
 
   render() {
     const temperature = this.state.temperature;
+    const scale = this.props.scale;
     return (
       <fieldset>
-        <legend>Enter temperature in Celsius:</legend>
+        <legend>Enter temperature in {scaleNames[scale]}:</legend>
         <input value={temperature} onChange={this.handleChange} />
-        <BoilingVerdict celsius={parseFloat(temperature)} />
       </fieldset>
     );
   }
